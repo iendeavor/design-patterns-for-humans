9f6a79801e7127d621d3a981bd04a3f35c4a9624
Ernest <iendeavor.github.io@gmail.com>
2021-08-07 21:15:54 +0800

    Update singleton.dart

--- singleton.dart
+++ singleton.dart
@@ -1,9 +1,16 @@
 class President {
+  factory President() {
+    return President.instance;
+  }
+
   President._();
 
   static President instance = President._();
 }
 
 void main() {
-  print(President.instance);
+  var president1 = President();
+  var president2 = President();
+
+  print(identical(president1, president2)); // true
 }
