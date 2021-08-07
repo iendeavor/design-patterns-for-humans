d9e132112ff948896f7a40c49d5d67a5d35a5cf0
Ernest <iendeavor.github.io@gmail.com>
2021-08-07 21:11:59 +0800

    Update singleton.dart

--- singleton.dart
+++ singleton.dart
@@ -1,22 +1,9 @@
-import 'dart:mirrors';
-
 class President {
-  factory President() {
-    return _instance;
-  }
+  President._();
 
-  static final President _instance = President._concrete();
-
-  President._concrete() {
-    InstanceMirror im = reflect(this);
-    if (im.type.reflectedType != President)
-      throw "President can't be inherited";
-  }
+  static President instance = President._();
 }
 
 void main() {
-  var president1 = President();
-  var president2 = President();
-
-  print(identical(president1, president2)); // true
+  print(President.instance);
 }
